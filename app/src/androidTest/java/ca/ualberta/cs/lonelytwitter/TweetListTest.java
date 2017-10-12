71230026adbd4434dd4fd4276c11ed948fed2679
hrai <hrai@ualberta.ca>
2017-10-11 19:14:59 -0600

    adding test cases

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -0,0 +1,54 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import static java.lang.Boolean.FALSE;
+
+/**
+ * Created by hrai on 10/11/17.
+ */
+
+public class TweetListTest extends ActivityInstrumentationTestCase2 {
+
+    public TweetListTest(){
+        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    }
+
+    public void testAddTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("adding tweet");
+        tweets.add(tweet);
+        assertTrue(tweets.hasTweet(tweet));
+    }
+
+    public void testDelete(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        tweets.add(tweet);
+        tweets.delete(tweet);
+        assertFalse(tweets.hasTweet(tweet));
+    }
+
+    public void testHasTweet(){
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.add(tweet);
+        assertTrue(list.hasTweet(tweet));
+    }
+
+    public void testGetTweet(){
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.add(tweet);
+        Tweet returnedTweet = list.getTweet(0);
+        assertEquals(returnedTweet.getMessage(), tweet.getMessage());
+    }
+
+    public void testGetCount(){
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("testing count");
+        list.add(tweet);
+        assertEquals(list.getCount(), 1);
+    }
+
+}
